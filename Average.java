a2f6d491c0b825afab82a5b16222b0b607a25aeb
lavender-aa <sodaissoda@gmail.com>
2025-01-20 23:01:39 -0500

    added file, put in header and mostly empty main function

---
+++ Average.java
@@ -0,0 +1,22 @@
+/*
+
+    Assignment 1: Average Program
+    Authors: Group 6
+        - Camron Mellott (mel98378@pennwest.edu)
+        - Lavender Wilson (wil81891@pennwest.edu)
+        - Nicola Razumic-Rushin (raz73517@pennwest.edu)
+
+*/
+
+
+import java.io.*;
+
+class Average
+{
+    public static void main(String[] args)
+    {
+        // create/instantiate variables used
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+    }
+}
