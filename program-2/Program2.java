757f1889d7dcb9135ca5f8b76eb7b69b4117c286
lavender-aa <sodaissoda@gmail.com>
2025-01-25 15:14:15 -0500

    added word class description

--- program-2/Program2.java
+++ program-2/Program2.java
@@ -1,5 +1,8 @@
 /*
 
+main function:
+--------------
+
 input file:
     - contains integers (r"(-?\d+)") and words (strings; r"([A-Za-z]+['\-\d]*)")
 output file:
@@ -14,6 +17,22 @@ notes:
     - use StringTokenizer to parse input file
     - for each word, either add to count (if exists) or create new Word, add to list
 
+
+more detailed steps:
+    - check inputs from command line
+    - validate files
+        - if any invalid, reprompt
+        - if any empty prompts, quit program
+    - open PrintWriter and FileReader
+    - create StringTokenizer
+    - while a line exists:
+        - while tokens in line:
+            - determine if token is word or int
+                - word: check word list, create word/add occurrence
+                - int: add to sum
+    - print each unique word to output, include number of occurrences
+    - print total number of unique words, sum of integers
+
 */
 
 class Program2 {
@@ -21,3 +40,23 @@ class Program2 {
         
     }
 }
+
+/* 
+
+word class:
+-----------
+
+data:
+    string word
+    int number of occurrences
+
+methods:
+    constructor (string)
+    get number of occurrences
+    get word
+    is equal to another word
+    is equal to another word (ignore case)
+    increase number of occurrences
+    index of word (string) in list of words
+
+*/
