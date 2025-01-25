a44bb691c144cf8eb966f4cf827450a5beced708
lavender-aa <sodaissoda@gmail.com>
2025-01-25 14:59:14 -0500

    added program 2 folder/file, short desc, empty class/main

---
+++ program-2/Program2.java
@@ -0,0 +1,23 @@
+/*
+
+input file:
+    - contains integers (r"(-?\d+)") and words (strings; r"([A-Za-z]+['\-\d]*)")
+output file:
+    - contains each unique word and number of occurrences
+    - ordered by first encountered
+    - end of list: total number of unique words, sum of integers
+
+
+notes:
+    - can enter input or input and output files in command line
+    - validate files; reprompt until valid or until nothing (quit)
+    - use StringTokenizer to parse input file
+    - for each word, either add to count (if exists) or create new Word, add to list
+
+*/
+
+class Program2 {
+    public static void main(String[] args) {
+        
+    }
+}
