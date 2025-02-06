bbbfbf4b9b89f066417df894ef519ca6aa398d2d
lavender-aa <sodaissoda@gmail.com>
2025-02-05 21:46:54 -0500

    added program comment header

--- program-2/Program2.java
+++ program-2/Program2.java
@@ -1,38 +1,10 @@
 /*
-
-main function:
---------------
-
-input file:
-    - contains integers (r"(-?\d+)") and words (strings; r"([A-Za-z]+['\-\d]*)")
-output file:
-    - contains each unique word and number of occurrences
-    - ordered by first encountered
-    - end of list: total number of unique words, sum of integers
-
-
-notes:
-    - can enter input or input and output files in command line
-    - validate files; reprompt until valid or until no input
-    - use StringTokenizer to parse input file
-    - for each word, either add to count (if exists) or create new Word, add to list
-
-
-more detailed steps:
-    - check inputs from command line
-    - validate files
-        - if any invalid, reprompt
-        - if any empty prompts, quit program
-    - open PrintWriter and FileReader
-    - create StringTokenizer
-    - while a line exists:
-        - while tokens in line:
-            - determine if token is word or int
-                - word: check word list, create word/add occurrence
-                - int: add to sum
-    - print each unique word to output, include number of occurrences
-    - print total number of unique words, sum of integers
-
+    Class: CMSC 3320 -- Technical Computing Using Java
+    Assignment 2: File Process
+    Authors: Group 6
+        - Camron Mellott (mel98378@pennwest.edu)
+        - Lavender Wilson (wil81891@pennwest.edu)
+        - Nicola Razumic-Rushin (raz73517@pennwest.edu)
 */
 
 import java.io.*;
