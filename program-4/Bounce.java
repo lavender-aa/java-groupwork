8ad39d6854c0cbab9448bca9f696e0c48e604725
lavender-aa <sodaissoda@gmail.com>
2025-02-18 09:21:43 -0500

    added folder and program file for program 4

---
+++ program-4/Bounce.java
@@ -0,0 +1,15 @@
+/*
+ * Program 4: Bounce
+ * Course: CMSC 3320 -- Technical Computing Using Java
+ * Authors: Group 6
+ *      - Lavender Wilson (wil81891@pennwest.edu)
+ *      - Camron Mellott (mel98378@pennwest.edu)
+ *      - Nicola Razumic-Rushin (raz73517@pennwest.edu)
+ */
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class Bounce {
+    
+}
