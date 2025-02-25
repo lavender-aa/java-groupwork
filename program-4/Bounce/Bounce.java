87cbb701541117e77c3ee71f7532f265f3eea7b2
lavender-aa <sodaissoda@gmail.com>
2025-02-25 09:17:25 -0500

    added constructor and helper functions

--- program-4/Bounce/Bounce.java
+++ program-4/Bounce/Bounce.java
@@ -10,6 +10,7 @@
 package Bounce;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
 
 public class Bounce extends Frame
 implements WindowListener, ComponentListener, ActionListener, AdjustmentListener, Runnable {
@@ -66,5 +67,234 @@ implements WindowListener, ComponentListener, ActionListener, AdjustmentListener
     Scrollbar speedScrollbar, sizeScrollbar;
     private Thread thread;
 
+    // constructor
+    public Bounce() {
+        setLayout(null);
+        setVisible(true);
+        calculateScreenSizes();
+        try {
+            initComponents();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        setElementPositions();
+        // startThread(); TODO: uncomment when startThread() written
+    }
 
+    void calculateScreenSizes() {
+        insets = getInsets();
+
+        // set screen width (has borders on left and right)
+        screenWidth = winWidth - insets.left - insets.right;
+
+        // set screen height (vertical insets, space at bottom for two rows of buttons)
+        screenHeight = winHeight - insets.top - insets.bottom - (2 * (BUTTONHEIGHT + BUTTONHEIGHTSPACING));
+        
+        // set frame size
+        setSize(winWidth, winHeight);
+
+        // calculate center, button width, button spacing
+        screenCenter = screenWidth / 2;
+        buttonWidth = screenWidth / 11; // 11 units
+        buttonSpacing = buttonWidth / 4;
+
+        // determine scroll bar width
+        scrollWidth = 2 * buttonWidth;
+
+        // recalculate max object size for screen
+        if(screenWidth >= screenHeight) { // limited by height
+            maxObjectSize = screenHeight - screenHeight/4;
+        }
+        else { // limited by width
+            maxObjectSize = screenWidth - screenWidth/4;
+        }
+
+        // set the background color
+        setBackground(Color.lightGray);
+    }
+
+    void initComponents() throws Exception, IOException {
+
+        // initialize program variables
+        paused = true;
+        run = true;
+        scrollSpeed = 50;
+        delay = (int) ((1.0/scrollSpeed) * SECONDS_TO_MILLIS);
+        
+        // create buttons
+        start = new Button("Run");
+        shape = new Button("Circle");
+        clear = new Button("Clear");
+        tail = new Button("No Tail");
+        quit = new Button("Quit");
+
+        // add buttons to the frame
+        add("Center", start);
+        add("Center", shape);
+        add("Center", clear);
+        add("Center", tail);
+        add("Center", quit);
+
+        // add actionListeners to buttons
+        start.addActionListener(this);
+        shape.addActionListener(this);
+        clear.addActionListener(this);
+        tail.addActionListener(this);
+        quit.addActionListener(this);
+
+        // create speed scroll bar
+        speedScrollbar = new Scrollbar(Scrollbar.HORIZONTAL);
+        speedScrollbar.setMaximum(SPEEDSCROLLMAX);
+        speedScrollbar.setMinimum(SPEEDSCROLLMIN);
+        speedScrollbar.setUnitIncrement(SCROLLUNIT);
+        speedScrollbar.setBlockIncrement(SCROLLBLOCK);
+        speedScrollbar.setValue(scrollSpeed);
+        speedScrollbar.setVisibleAmount(SPEEDSCROLLVISIBLE);
+        speedScrollbar.setBackground(Color.gray);
+
+        // create size scroll bar
+        sizeScrollbar = new Scrollbar(Scrollbar.HORIZONTAL);
+        sizeScrollbar.setMaximum(maxObjectSize);
+        sizeScrollbar.setMinimum(MINOBJECTSIZE);
+        sizeScrollbar.setUnitIncrement(SCROLLUNIT);
+        sizeScrollbar.setBlockIncrement(SCROLLBLOCK);
+        sizeScrollbar.setValue(objectSize);
+        sizeScrollbar.setVisibleAmount(SIZESCROLLVISIBLE);
+        sizeScrollbar.setBackground(Color.gray);
+
+        // create object TODO: uncomment when object class written
+        // object = new Objc(objectSize, maxObjectSize, screenWidth, screenHeight);
+        // object.setBackground(Color.white);
+
+        // add scrollbars, labels, object to frame
+        add(speedScrollbar);
+        add(sizeScrollbar);
+        add(speedLabel);
+        add(sizeLabel);
+        // add(object); TODO: uncomment when object class written
+
+        // add listeners to scrollbars
+        speedScrollbar.addAdjustmentListener(this);
+        sizeScrollbar.addAdjustmentListener(this);
+
+        // add listeners to the frame
+        this.addComponentListener(this);
+        this.addWindowListener(this);
+
+        // set sizes, bounds, validate layout
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setMinimumSize(getPreferredSize());
+        setBounds(winLeft, winTop, WIDTH, HEIGHT);
+        validate();
+    }
+
+    void setElementPositions() {
+
+        // set button positions
+        start.setLocation(
+            screenCenter - 2*(buttonWidth+buttonSpacing) - buttonWidth/2,
+            screenHeight + BUTTONHEIGHTSPACING + insets.top
+        );
+        shape.setLocation(
+            screenCenter - buttonWidth - buttonSpacing - buttonWidth/2,
+            screenHeight + BUTTONHEIGHTSPACING + insets.top
+        );
+        tail.setLocation(
+            screenCenter - buttonWidth/2,
+            screenHeight + BUTTONHEIGHTSPACING + insets.top
+        );
+        clear.setLocation(
+            screenCenter + buttonSpacing + buttonWidth/2,
+            screenHeight + BUTTONHEIGHTSPACING + insets.top
+        );
+        quit.setLocation(
+            screenCenter + buttonWidth + 2*buttonSpacing + buttonWidth/2,
+            screenHeight + BUTTONHEIGHTSPACING + insets.top
+        );
+
+        // set button sizes
+        start.setSize(buttonWidth, BUTTONHEIGHT);
+        shape.setSize(buttonWidth, BUTTONHEIGHT);
+        tail.setSize(buttonWidth, BUTTONHEIGHT);
+        clear.setSize(buttonWidth, BUTTONHEIGHT);
+        quit.setSize(buttonWidth, BUTTONHEIGHT);
+
+        // set scrollbar positions
+        speedScrollbar.setLocation(
+            insets.left + buttonSpacing,
+            screenHeight + BUTTONHEIGHTSPACING + insets.top
+        );
+        sizeScrollbar.setLocation(
+            winWidth - scrollWidth - insets.right - buttonSpacing,
+            screenHeight + BUTTONHEIGHTSPACING + insets.top
+        );
+
+        // set scrollbar sizes
+        speedScrollbar.setSize(scrollWidth, SCROLLBARHEIGHT);
+        sizeScrollbar.setSize(scrollWidth, SCROLLBARHEIGHT);
+
+        // set label positions
+        speedLabel.setLocation(
+            insets.left + buttonSpacing,
+            screenHeight + BUTTONHEIGHTSPACING + BUTTONHEIGHT + insets.top
+        );
+        sizeLabel.setLocation(
+            winWidth - scrollWidth - insets.right - buttonSpacing,
+            screenHeight + BUTTONHEIGHTSPACING + BUTTONHEIGHT + insets.top
+        );
+
+        // set label sizes
+        speedLabel.setSize(scrollWidth, BUTTONHEIGHT);
+        sizeLabel.setSize(scrollWidth, SCROLLBARHEIGHT);
+
+        // set object bounds
+        // object.setBounds(insets.left, insets.top, screenWidth, screenHeight); TODO: uncomment when object class written
+
+    }
+
+    @Override
+    public void run() {}
+
+    @Override
+    public void adjustmentValueChanged(AdjustmentEvent e) {}
+
+    @Override
+    public void actionPerformed(ActionEvent e) {}
+
+    @Override
+    public void componentHidden(ComponentEvent e) {}
+
+    @Override
+    public void componentMoved(ComponentEvent e) {}
+
+    @Override
+    public void componentResized(ComponentEvent e) {}
+
+    @Override
+    public void componentShown(ComponentEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'componentShown'");
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {}
+
+    @Override
+    public void windowClosed(WindowEvent e) {}
+
+    @Override
+    public void windowClosing(WindowEvent e) {}
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {}
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {}
+
+    @Override
+    public void windowIconified(WindowEvent e) {}
+
+    @Override
+    public void windowOpened(WindowEvent e) {}
 }
