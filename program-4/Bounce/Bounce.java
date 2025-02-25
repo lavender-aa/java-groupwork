6ef80c34db9f5b8950bb6e9466c8fc1d627956f8
lavender-aa <sodaissoda@gmail.com>
2025-02-25 09:14:02 -0500

    added variables

--- program-4/Bounce/Bounce.java
+++ program-4/Bounce/Bounce.java
@@ -11,6 +11,60 @@ package Bounce;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Bounce {
+public class Bounce extends Frame
+implements WindowListener, ComponentListener, ActionListener, AdjustmentListener, Runnable {
     
+    // debug TODO: remove
+    void print(String s) {
+        System.out.println(s);
+    }
+    
+    // serial UID
+    private static final long serialVersionUID = 10L;
+
+    // constants
+    private final int WIDTH = 640;  // initial frame width
+    private final int HEIGHT = 400; // initial frame height
+    private final int BUTTONHEIGHT = 30; // button height
+    private final int BUTTONHEIGHTSPACING = 5; // button height spacing
+    private final int MINOBJECTSIZE = 10;
+    private final int DEFAULTOBJECTSIZE = 21;
+    private final int SPEEDSCROLLVISIBLE = 50;
+    private final int SIZESCROLLVISIBLE = 25;
+    private final int SPEEDSCROLLMIN = 1;
+    private final int SPEEDSCROLLMAX = 300;
+    private final int SCROLLUNIT = 10; // unit step size
+    private final int SCROLLBLOCK = 50; // block step size
+    private final int SCROLLBARHEIGHT = BUTTONHEIGHT;
+    private final double SECONDS_TO_MILLIS = 1000;
+
+    // primitives + strings
+    private int winWidth = WIDTH;
+    private int winHeight = HEIGHT;
+    private int winTop = 10;  // top of frame
+    private int winLeft = 10; // left side of frame
+    private int screenWidth;
+    private int screenHeight;
+    private int screenCenter = WIDTH / 2;
+    private int buttonWidth = 50;
+    private int buttonSpacing = buttonWidth / 4;
+    private int maxObjectSize = 500;
+    private int objectSize = DEFAULTOBJECTSIZE;
+    private int scrollWidth;
+    private boolean run; // control program loop
+    private boolean paused; // control running vs paused
+    private static boolean started; // control animation
+    private int scrollSpeed;
+    private int delay; // current time delay
+    
+    // objects
+    private Insets insets;
+    Button start, shape, clear, tail, quit;
+    // private Objc object; TODO: uncomment when object class written
+    private Label speedLabel = new Label("Speed", Label.CENTER);
+    private Label sizeLabel = new Label("Size", Label.CENTER);
+    Scrollbar speedScrollbar, sizeScrollbar;
+    private Thread thread;
+
+
 }
